905bc88b6d24dd4782da2a80538d041c132f024a
SHASHANK S K <quattroz2432@gmail.com>
2022-01-30 09:14:18 +0530

    it was a temp file

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-newContactButton = driver.find_element_by_xpath(
-#     '//*[@id="app"]/div/div[2]/div[2]/div[1]/div/div/div/div/div/div[1]/div[2]/div/div/div/span/button[1]')
-# newContactButton.click()
-# newContactButton.click()
-# newContactButton.click()
-# newContactButton.click()
